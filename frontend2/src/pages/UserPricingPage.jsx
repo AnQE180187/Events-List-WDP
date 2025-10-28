f8897112bce37202f6da078126e81af5e9110515
khanh gia <luonggiakhanh1997@gmail.com>
2025-10-28 14:06:54 +0700

    update description

--- frontend2/src/pages/UserPricingPage.jsx
+++ frontend2/src/pages/UserPricingPage.jsx
@@ -12,7 +12,6 @@ const UserPricingPage = () => {
       popular: true,
       features: [
         'Truy cập Chatbot AI (Beta)',
-        'Gợi ý sự kiện được cá nhân hóa',
         'Huy hiệu "Người dùng VIP" trên hồ sơ',
       ],
     },
